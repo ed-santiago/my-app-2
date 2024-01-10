f643d523d6487bf57a431335ac3a7f3fb4751a7c
Edmond <edmond.santiago7@gmail.com>
2024-01-10 17:41:59 +1100

    idk

--- src/Users.js
+++ src/Users.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-function Users() {
+function Posts() {
+  const [usersArray, setUsersArray] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://social-media-project-s52o.onrender.com/users`)
+      .then((r) => r.json())
+      .then((users) => setUsersArray(users))
+  }, [])
+
+  const element = <FontAwesomeIcon icon={faCoffee} />
+
+  const usersElement = usersArray.map((user) => {
+    return <h2>{user.name}</h2>
+  });
+
   return (
-    <h1>users</h1>
+    <div>
+      {element}
+      {usersElement}
+      <h1>will this work?</h1>
+    </div>
   );
 }
 
-export default Users;
+export default Posts;
